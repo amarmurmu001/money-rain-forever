3e62fd378cf47168b4ff5efde81db0f96cf8d43a
amarmurmu001 <iron57441@gmail.com>
2025-07-22 15:37:10 +0530

    add: menu active animation

--- src/components/ui/navbar-menu.tsx
+++ src/components/ui/navbar-menu.tsx
@@ -1,33 +1,47 @@
-import { motion } from "motion/react";
+import { useState } from "react";
+import { motion } from "framer-motion";
+
 const NavBar = () => {
+  const [active, setActive] = useState("Home");
+
+  const menuItems = ["Home", "Product", "Company", "Contact", "Terms"];
+
   return (
     <motion.nav
       initial={{ opacity: 0, y: -100 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.4 }}
-      className="bg-[#070707] absolute left-1/2 -translate-x-1/2 shadow-md rounded-full  py-4 px-4 flex items-center justify-between w-[80vw] mx-auto mt-4"
+      className="bg-[#070707] absolute left-1/2 -translate-x-1/2 shadow-md rounded-full py-4 px-4 flex items-center justify-between w-[80vw] mx-auto mt-4"
     >
+      {/* Brand */}
       <div className="font-bold text-2xl px-5 text-[#7DF009]">MRF</div>
-      <div>
-        <ul className="flex space-x-10 text-[14px] ">
-          <li className="text-white cursor-pointer hover:text-[#7DF009]">
-            Home
+
+      {/* Menu with sliding background */}
+      <ul className="flex space-x-4 relative text-[14px]">
+        {menuItems.map((item) => (
+          <li
+            key={item}
+            onClick={() => setActive(item)}
+            className="relative px-2 py-2 cursor-pointer text-white"
+          >
+            {active === item && (
+              <motion.div
+                layoutId="menu-bg"
+                className="absolute inset-0 border-b-2 border-[var(--green)]  z-0"
+                transition={{ type: "spring", stiffness: 300, damping: 30 }}
+              />
+            )}
+            <span
+             
+            >
+              {item}
+            </span>
           </li>
-          <li className="text-white cursor-pointer hover:text-[#7DF009]">
-            Product
-          </li>
-          <li className="text-white cursor-pointer hover:text-[#7DF009]">
-            Company
-          </li>
-          <li className="text-white cursor-pointer hover:text-[#7DF009]">
-            Contact
-          </li>
-          <li className="text-white cursor-pointer hover:text-[#7DF009]">
-            Terms
-          </li>
-        </ul>
-      </div>
-      <button className="px-5 text-[14px] py-2 bg-[#7DF009] text-black rounded-full">
+        ))}
+      </ul>
+
+      {/* CTA Button */}
+      <button className="px-5 text-[14px] py-2 cursor-pointer bg-[#7DF009] text-black rounded-full">
         Buy Access
       </button>
     </motion.nav>
